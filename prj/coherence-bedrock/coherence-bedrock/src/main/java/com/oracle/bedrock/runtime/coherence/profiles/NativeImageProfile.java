4f3bcff1274bc1ef370a43843abe33559f4c0dfa
Jonathan Knight <jk5766@outlook.com>
2025-07-31 10:16:39 +0300

    Adding GraalVM native image support
    # Conflicts:
    #	prj/coherence-testing-support/src/main/java/com/oracle/coherence/testing/AbstractTestInfrastructure.java

---
+++ prj/coherence-bedrock/coherence-bedrock/src/main/java/com/oracle/bedrock/runtime/coherence/profiles/NativeImageProfile.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
+
+package com.oracle.bedrock.runtime.coherence.profiles;
+
+import com.oracle.bedrock.Option;
+import com.oracle.bedrock.OptionsByType;
+
+import com.oracle.bedrock.runtime.Application;
+import com.oracle.bedrock.runtime.ApplicationLauncher;
+import com.oracle.bedrock.runtime.MetaClass;
+import com.oracle.bedrock.runtime.Platform;
+import com.oracle.bedrock.runtime.Profile;
+
+import com.oracle.bedrock.runtime.coherence.LocalNativeImageLauncher;
+
+import com.oracle.bedrock.runtime.java.ClassPath;
+
+import com.oracle.bedrock.runtime.options.Executable;
+
+/**
+ * A Bedrock profile to enable running GraalVM native images
+ */
+public class NativeImageProfile
+        implements Profile, Option
+    {
+    @Override
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public <A extends Application> ApplicationLauncher<A> getLauncher(MetaClass<A> metaClass)
+        {
+        return new LocalNativeImageLauncher();
+        }
+
+    /**
+     * Create a {@link NativeImageProfile}.
+     */
+    @OptionsByType.Default
+    public NativeImageProfile(String nativeImage)
+        {
+        f_nativeImage = nativeImage;
+        }
+
+    @Override
+    public void onLaunching(Platform platform, MetaClass metaClass, OptionsByType options)
+        {
+        options.add(Executable.named(f_nativeImage));
+        options.remove(ClassPath.class);
+        }
+
+    @Override
+    public void onLaunched(Platform platform, Application application, OptionsByType optionsByType)
+        {
+        }
+
+    @Override
+    public void onClosing(Platform platform, Application application, OptionsByType optionsByType)
+        {
+        }
+
+    // ----- data members ---------------------------------------------------
+
+    private final String f_nativeImage;
+    }
